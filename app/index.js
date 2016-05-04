1197e9ed250b4b89c015d6df20168e711488ab9b
frank <frank.lood@fast2.se>
2016-05-05 00:28:49 +0200

    testing

--- app/index.js
+++ app/index.js
@@ -3,10 +3,14 @@ var http = require('http');
 
 //Lets define a port we want to listen to
 const PORT = process.env.OPENSHIFT_NODEJS_PORT || 1337; 
+// const PORT = 7331; 
 
 //We need a function which handles requests and send response
 function handleRequest( request, response ){
-    response.write ()
+    response.writeHead( 418, {
+        'Content-Type': 'text/plain' 
+    } );
+    response.write( 'WAT' );
     response.end( 'Obey: ' + request.url);
 }
 
